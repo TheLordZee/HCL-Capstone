a336795314212f0713f45502a1fef138c3df9588
TheLordZee <l0rclz33@gmail.com>
2022-01-18 15:16:10 -0500

    ProjectItem and Header components

--- hcl-frontend/src/components/Dashboard.js
+++ hcl-frontend/src/components/Dashboard.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Header from './layout/Header';
+import ProjectItem from './project/ProjectItem';
 
 function Dashboard() {
     return (
-        <div>
+        <div className='dashboard'>
+            <Header/>
             <h1>Welcome to the Dashboard</h1>
+            <ProjectItem/>
+            <ProjectItem/>
+            <ProjectItem/>
+            <ProjectItem/>
+            <ProjectItem/>
         </div>
     )
 }
---
+++ hcl-frontend/src/components/project/ProjectItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProjectItem() {
+    return (
+        <div className='project'>
+            <h1>Project Item</h1>
+        </div>
+    )
+}
+
+export default ProjectItem;
