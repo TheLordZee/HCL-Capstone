e4590c33c0432f3fe0425f8de1ebb6d1c8a94c35
TheLordZee <l0rclz33@gmail.com>
2022-01-19 13:40:40 -0500

    Redux integration

--- hcl-frontend/src/App.js
+++ hcl-frontend/src/App.js
@@ -2,14 +2,18 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Routes from './Routes'
 import {BrowserRouter} from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from "./store"
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className="App">
-       <Routes/>
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="App">
+         <Routes/>
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
