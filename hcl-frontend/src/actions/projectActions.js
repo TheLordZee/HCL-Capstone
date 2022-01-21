0fc5ab6b7dbcffc0ac6902b95ab1c9d2912d2864
TheLordZee <l0rclz33@gmail.com>
2022-01-21 17:20:34 -0500

    types and reducers for project tasks

--- hcl-frontend/src/actions/projectActions.js
+++ hcl-frontend/src/actions/projectActions.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 import { GET_ERRORS, GET_PROJECT, GET_PROJECTS, DELETE_PROJECT } from "./types";
 
-const BASE_URL = "http://localhost:8080";
-
 export const createProject = (project, history) => async dispatch => {
     try {
         const res = await axios.post(`/api/project`, project)
