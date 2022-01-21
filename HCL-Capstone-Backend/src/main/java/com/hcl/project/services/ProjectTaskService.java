e466a1bd662d8c841cd8c4b02d267b62140a938f
TheLordZee <l0rclz33@gmail.com>
2022-01-20 19:32:05 -0500

    mySql and handling project not found exception

--- HCL-Capstone-Backend/src/main/java/com/hcl/project/services/ProjectTaskService.java
+++ HCL-Capstone-Backend/src/main/java/com/hcl/project/services/ProjectTaskService.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hcl.project.domain.Backlog;
+import com.hcl.project.domain.Project;
 import com.hcl.project.domain.ProjectTask;
+import com.hcl.project.exceptions.ProjectIdException;
+import com.hcl.project.exceptions.ProjectNotFoundException;
 import com.hcl.project.repositories.BacklogRepository;
+import com.hcl.project.repositories.ProjectRepository;
 import com.hcl.project.repositories.ProjectTaskRepository;
 
 @Service
@@ -19,30 +23,45 @@ public class ProjectTaskService {
 	@Autowired
 	private ProjectTaskRepository projectTaskRepository;
 	
+	@Autowired
+	private ProjectRepository projectRepository;
+	
 	public ProjectTask addProjectTask(String projectIdentifier, ProjectTask pt) {
-		Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
-		pt.setBacklog(backlog);
-		int btSeq = backlog.getPTSequence() + 1;
-		
-		backlog.setPTSequence(btSeq);
-		
-		pt.setProjectSequence(projectIdentifier + "-" + btSeq);
-		pt.setProjectIdentifier(projectIdentifier);
-		
-		
-		if(pt.getPriority()==0) {
-			pt.setPriority(3);
+		try {
+			Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+			pt.setBacklog(backlog);
+			int btSeq = backlog.getPTSequence() + 1;
+			
+			backlog.setPTSequence(btSeq);
+			
+			pt.setProjectSequence(projectIdentifier + "-" + btSeq);
+			pt.setProjectIdentifier(projectIdentifier);
+			
+			
+			if(pt.getPriority()==0) {
+				pt.setPriority(3);
+			}
+			if(pt.getStatus()=="" || pt.getStatus()==null) {
+				pt.setStatus("TO_DO");
+			}
+			
+			return projectTaskRepository.save(pt);
+
+		}catch(Exception e) {
+			throw new ProjectNotFoundException("Project with ID: '" + projectIdentifier + "' Does Not Exist!");
 		}
-		if(pt.getStatus()=="" || pt.getStatus()==null) {
-			pt.setStatus("TO_DO");
-		}
-		
-		return projectTaskRepository.save(pt);
 	}
 
-	public Iterable<ProjectTask> findBackLogById(String backlog_id) {
+	public Iterable<ProjectTask> findBackLogById(String id) {
 		// TODO Auto-generated method stub
-		return projectTaskRepository.findByProjectIdentifierOrderByPriority(backlog_id);
+		
+		Project project = projectRepository.findByProjectIdentifier(id);
+		
+		if(project==null) {
+			throw new ProjectNotFoundException("Project with ID: '" + id + "' Not Found");
+		}
+		
+		return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
 	}
 	 
 }
--- HCL-Capstone-Backend/src/main/java/com/hcl/project/web/ProjectController.java
+++ HCL-Capstone-Backend/src/main/java/com/hcl/project/web/ProjectController.java
@@ -34,7 +34,8 @@ public class ProjectController {
 	@Autowired
 	private ProjectService projectService;
 	
-	@Autowired MapValidationErrorService mapValidationErrorService;
+	@Autowired 
+	MapValidationErrorService mapValidationErrorService;
 	
 	@PostMapping("")
 	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
@@ -43,7 +44,7 @@ public class ProjectController {
 		if(errorMap != null) return errorMap;
 		
 		Project project1 = projectService.saveOrUpdateProject(project);
-		return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+		return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
 	}
 	
 	@GetMapping("/{projectId}")
